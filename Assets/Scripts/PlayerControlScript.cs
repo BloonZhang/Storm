5e892959017d0774c026bbd18994f224d28d55ac
Bloon <BloonZhang@gmail.com>
2021-05-01 11:49:07 -0500

    player movement

---
+++ Assets/Scripts/PlayerControlScript.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControlScript : MonoBehaviour
+{
+    // ***** Variables *****
+    // GameObject variables
+
+    // Component variables. Set in Awake()
+    // Comopnents are the things you add to gameobjects in the unity editor, 
+    // like SpriteRenderer, CircleCollider2D, RigidBody2D, etc.
+    private Rigidbody2D my_Rigidbody2D;
+
+    // Editable variables
+    // Change these in the unity inspector. Changing it here will only change the default value
+     public float moveSpeedMultiplier = 5f;
+
+    // helper varaibles
+    private float horizontalMove = 0f;
+    private float verticalMove = 0f;
+    private Vector3 m_Velocity = Vector3.zero; // This is used in Move(). Copied from a tutorial
+
+    // ***** MonoBehaviour methods *****
+    // Awake is called before the first frame update
+    // Put setup code here
+    void Awake()
+    {
+        // TODO: turn class into a singleton
+        // Set up GameObject/Component variables
+        my_Rigidbody2D = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    // Put input-related code here
+    void Update()
+    {
+        // Get player input for movement. This will be used in FixedUpdate()
+        horizontalMove = Input.GetAxisRaw("Horizontal") * moveSpeedMultiplier;
+        verticalMove = Input.GetAxisRaw("Vertical") * moveSpeedMultiplier;
+        
+    }
+
+    // FixedUpdate is called in-step with the physics engine
+    // Put physics-related code here
+    void FixedUpdate()
+    {
+        // send horizontalMove and verticalMove (updated in Update()) to Move()
+        Move(horizontalMove, verticalMove);
+    }
+
+
+    // ***** Self created methods *****
+    // helper methods
+    void Move(float horizontalMove, float verticalMove)
+    {
+        // This was copied from a tutorial.
+        // Movement is done through the physics engine, not manually changing the position
+        Vector3 targetVelocity = new Vector2(horizontalMove, verticalMove);
+        my_Rigidbody2D.velocity = Vector3.SmoothDamp(my_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, 0.05f);
+    }
+}
