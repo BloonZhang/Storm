21c8206f6e038731be5800dd2a6ea61ba4fb731f
iobg <iobg0320@gmail.com>
2021-05-02 23:01:34 -0400

    added inventoryController, will we be able to use ScriptableObjects dynamically?

--- Assets/Scripts/Controllers/InventoryController.cs
+++ Assets/Scripts/Controllers/InventoryController.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class InventoryController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   public List<Item> inventory = new List<Item>();
 }
